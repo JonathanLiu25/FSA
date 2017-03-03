3884c782d075fd9959bea92e93e077f78f54b896
johnptmcdonald <johnptmcdonald@gmail.com>
2017-03-03 17:01:52 -0500

    adds sorting solutions

---
+++ 01-junior-phase/03-sorting/solution-es6/performance.js
@@ -0,0 +1,24 @@
+for (var i = 9; i <= 12; i++) {
+  const numItems = Math.pow(2, i);
+  const nativeTestArray = [];
+
+  for(let j = 0; j < numItems; j++) {
+    let rand = Math.floor(Math.random() * numItems);
+    nativeTestArray.push(rand);
+  }
+
+  const bTestArray = nativeTestArray.slice(0);
+  const mTestArray = nativeTestArray.slice(0);
+
+  console.time(numItems + ' native');
+  nativeTestArray.sort(function(a, b){ return a - b; });
+  console.timeEnd(numItems + ' native');
+
+  console.time(numItems + ' bubble');
+  bubbleSort(bTestArray);
+  console.timeEnd(numItems + ' bubble');
+
+  console.time(numItems + ' merge');
+  mergeSort(mTestArray);
+  console.timeEnd(numItems + ' merge');
+}
