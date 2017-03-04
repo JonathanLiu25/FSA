59a6eac00b66364db3464d483446446c222deb3b
John McDonald <johnptmcdonald@gmail.com>
2017-03-03 21:17:37 -0500

    remove comments from intro-to-node

--- 01-junior-phase/05-intro-to-node/intro-to-node/house.js
+++ 01-junior-phase/05-intro-to-node/intro-to-node/house.js
@@ -1,17 +1,6 @@
 var Door = require('./Door.js');
 var Wall = require('./Wall.js')
 
-// var Wall = require('./Wall.js').WallFunction;
-// var someOtherFunction = require('./Wall.js').someOtherFunction;
-
-// DEMO BUILTIN:
-// var path = require('path')
-// console.log(path.join(__dirname, '../../'))
-
-
-// DEMO CHALK - need to have npm init and npm i --save chalk first
-// var chalk = require('chalk')
-// console.log(chalk.blue('DOGS RULE OK'))
 
 function House(){
 	this.wall = new Wall(10,10);
@@ -21,29 +10,3 @@ function House(){
 
 console.log(new House())
 
-
-// console.log(someOtherFunction())
-
-
-// qLIOeqGBbWRepzixT5kw9VxIK
-// 141QzwabbWPtAeqbmW2SalcrGRvOwUJBeLtKNcTfsTBVUOKhIs
-// 807112520-9oCDrkKYrQKhlEGgGxlWfu7AGO9mmCHl20egMz0W
-// H6RiuLwVBglXRaBftGOKEh71VQYnECiQ1SaYZH3OsnwqT
-
-// var Twitter = require('twitter');
- 
-// var client = new Twitter({
-//   consumer_key: 'qLIOeqGBbWRepzixT5kw9VxIK',
-//   consumer_secret: '141QzwabbWPtAeqbmW2SalcrGRvOwUJBeLtKNcTfsTBVUOKhIs',
-//   access_token_key: '807112520-9oCDrkKYrQKhlEGgGxlWfu7AGO9mmCHl20egMz0W',
-//   access_token_secret: 'H6RiuLwVBglXRaBftGOKEh71VQYnECiQ1SaYZH3OsnwqT'
-// });
- 
-// var params = {screen_name: 'johnptmcdonald'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     tweets.forEach(function(tweet){
-//     	console.log(tweet.text)
-//     })
-//   }
-// });
