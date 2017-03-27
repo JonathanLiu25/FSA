33d253034ca5e030670035f02fe68d6bb1418aed
Omri Bernstein <hippiccolo@gmail.com>
2017-03-27 11:39:05 -0400

    React lecture demo code starting point

---
+++ 01-junior-phase/18-juke/1-react/lecture-demo/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './browser/react-app.js',
+  output: {
+    path: __dirname,
+    filename: './bundle.js'
+  },
+  context: __dirname,
+  module: {
+    loaders: [{
+      test: /jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
