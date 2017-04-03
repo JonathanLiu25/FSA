4bbadadf8830bb69e7953b1e655e5eaa31c8bc1a
Omri Bernstein <hippiccolo@gmail.com>
2017-04-03 10:09:40 -0400

    React redux lecture demo code starting point

---
+++ 01-junior-phase/18-juke/5-react-redux/lecture-demo/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+
+// create a server
+const app = express();
+
+// logging middleware
+app.use(volleyball);
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// static file serving middleware
+app.use(express.static(__dirname));
+
+// our data model, no database (for simplicity), just this array
+let incrementingId = 1;
+const diaryEntries = [];
+
+let currentEntry = {};
+
+app.get('/api/diary/current', function (req, res) {
+  res.json(currentEntry);
+});
+
+app.put('/api/diary/current', function (req, res) {
+  currentEntry = req.body;
+  res.status(204).end();
+});
+
+// all entries
+app.get('/api/diary', function (req, res, next) {
+  res.json(diaryEntries);
+});
+
+// create a new entry
+app.post('/api/diary', function (req, res, next) {
+  req.body.id = incrementingId++;
+  diaryEntries.push(req.body);
+  res.status(201).end();
+});
+
+// update an existing entry
+app.put('/api/diary/:entryId', function (req, res, next) {
+  const entry = diaryEntries.find(elem => elem.id === req.params.entryId);
+  Object.assign(entry, req.body);
+  res.status(204).end();
+});
+
+// delete an existing entry
+app.delete('/api/diary/:entryId', function (req, res, next) {
+  const entryIndex = diaryEntries.findIndex(elem => elem.id === req.params.entryId);
+  diaryEntries.splice(entryIndex, 1);
+  res.status(204).end();
+});
+
+// start the server
+const port = 8080;
+app.listen(port, function (err) {
+  if (err) {
+    console.error('I failed to start the server, how disappointing');
+    console.error(err);
+  } else {
+    console.log(`Listening! Connect at http://localhost:${port}`);
+  }
+})
